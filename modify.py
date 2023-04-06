19d3a31c7ad626e59debdc2cae9334792b0bcd98
Derek Avila <derekavila1086@gmail.com>
2023-04-06 10:49:49 -0500

    replace random sentences in sample.txt

--- modify.py
+++ modify.py
@@ -1,6 +1,7 @@
 import sys
 import input_validation
 import nltk
+import random
 
 # Reads the file and splits the result into its respective sentences
 def get_sentences(filename:str):
@@ -9,8 +10,20 @@ def get_sentences(filename:str):
     file_contents = infile.read()
 
     # Split the file into sentences
-    nltk.download('punkt')
+    nltk.download('punkt')  
     return nltk.sent_tokenize(file_contents)
+
+def replace_random_sentence(sentences):
+    # Select a random sentence to replace
+    sentence_to_replace = random.choice(sentences)
+
+    # Generate a random sentence to replace it
+    new_sentence = "This is a new random sentence."
+
+    # Replace the selected sentence with the new one
+    sentences[sentences.index(sentence_to_replace)] = new_sentence
+
+    return sentences
 
 if __name__ == "__main__":
     # Check the validity of arguments
@@ -18,3 +31,9 @@ if __name__ == "__main__":
     input_validation.check_file_valid(sys.argv[1])
 
     sentences = get_sentences(sys.argv[1])
+    new_sentences = replace_random_sentence(sentences)
+    
+    # Print the new sentences
+    for sentence in new_sentences:
+        print(sentence)
+        print() # Add a blank line for readability
