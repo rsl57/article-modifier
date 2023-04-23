79156bca66bdef53aa69e48d2ab281f9a9ff7037
Chimpy <94925923+ConteGit@users.noreply.github.com>
2023-04-23 02:55:23 -0500

    Create testfile.py

---
+++ testfile.py
@@ -0,0 +1,19 @@
+import unittest
+
+class Testcheck_args(unittest. Testcase): 
+  
+  def test_num_of_args(self):
+    self.assertTrue num_of_args(2)
+    self.assertFalse num_of_args(-1)
+    self.assertFalse num_of_args(0)
+    
+class Testget_sentence(unittest.TestCase):
+  
+  def test_split_sentences(self):
+    test_content = 
+    
+
+    
+    
+if__name__ == '__main__':
+  unittest.main()
